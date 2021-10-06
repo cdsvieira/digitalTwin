ac27d072d55ced11bc4e18246c3f7153d96fff8b
cdsvieira <35815812+cdsvieira@users.noreply.github.com>
2021-10-07 00:57:03 +0100

    Added docker / deploy configuration

--- src/main/java/com/koerber/digitalTwin/DigitalTwinApplication.java
+++ src/main/java/com/koerber/digitalTwin/DigitalTwinApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DigitalTwinApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(DigitalTwinApplication.class, args);
 	}
 
