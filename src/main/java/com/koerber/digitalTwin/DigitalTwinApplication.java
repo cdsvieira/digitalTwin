21dc050a87e35dcb2c5eed9e9632811d290dd088
cdsvieira <35815812+cdsvieira@users.noreply.github.com>
2021-10-06 23:05:31 +0100

    initial commit - spring initializr

---
+++ src/main/java/com/koerber/digitalTwin/DigitalTwinApplication.java
@@ -0,0 +1,13 @@
+package com.koerber.digitalTwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalTwinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koerber/digitalTwin/DigitalTwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koerber.digitalTwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalTwinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
